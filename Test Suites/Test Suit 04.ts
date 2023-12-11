a719b6a2a3c20a7755f901a5fe912de6b0498c67
1878 <1878@192.168.10.118>
2023-12-12 02:08:20 +0700

    bb

---
+++ Test Suites/Test Suit 04.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suit 04</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f5c901f2-8baa-45bf-9272-60cc77f4d56f</testSuiteGuid>
+</TestSuiteEntity>
